be62cf130b4d72cb39ec6396ebe2535f8cacafb8
JoeyMai99 <mait4@student.douglascollege.ca>
2020-02-14 13:49:48 -0800

    Midterm

---
+++ Winter2020CSIS3275Midterm/src/HelloWorld.java
@@ -0,0 +1,9 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Thi Tuyet Nhi Mai");
+		System.out.println("mait4@student.douglascollege.ca");
+	}
+
+}
